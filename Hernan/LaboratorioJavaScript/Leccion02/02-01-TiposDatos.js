fc488ab408ea023259fa429ddbe0775a8205fdf7
Elher <hervega123@gmail.com>
2023-09-07 02:20:17 -0300

    Añadida carpeta Leccion02 / Clase 3 JavaScript

---
+++ Hernan/LaboratorioJavaScript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,16 @@
+// Tipos de datos en JavaScript
+/*
+La sintaxis en JavaScript
+es muy parecida a Java en cuanto a comentarios
+se podría decir que es idéntica
+*/
+var nombre = "Ariel"; // Tipo Str
+console.log(nombre);
+var numero = 3000; // Tipo numérico
+console.log(numero);
+var objeto = {
+    nombre: "Ariel",
+    apellido: "Betancud",
+    telefono: "123456789"
+}
+console.log(objeto);
