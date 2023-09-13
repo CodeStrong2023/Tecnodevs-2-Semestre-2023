4b12ae953d35b70d360c04c226f0c369582eeb6e
Elher <hervega123@gmail.com>
2023-09-13 14:29:57 -0300

    Añadí Tipos de Datos Parte 2, Clase 4, JavaScript

--- Hernan/LaboratorioJavaScript/Leccion02/02-01-TiposDatos.js
+++ Hernan/LaboratorioJavaScript/Leccion02/02-01-TiposDatos.js
@@ -5,7 +5,11 @@ es muy parecida a Java en cuanto a comentarios
 se podría decir que es idéntica
 */
 var nombre = "Ariel"; // Tipo Str
-console.log(nombre);
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.5;
+console.log(typeof nombre)
 var numero = 3000; // Tipo numérico
 console.log(numero);
 var objeto = {
@@ -14,3 +18,27 @@ var objeto = {
     telefono: "123456789"
 }
 console.log(objeto);
+
+// Tipo de dato booleano (Bandera)
+
+var bandera = true;
+console.log(bandera);
+
+// Tipo de dato función
+
+function miFuncion (){};
+console.log(typeof miFuncion);
+
+//Tipo de dato symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo de dato Clase
+
+class persona {
+    constructor(nombre,apellido){
+        this.nombre = nombre;   // this. hace referencia a los atributos de nuestra clase
+        this.apellido = apellido;
+    }
+}
+console.log(typeof persona) // Una clase es una función
