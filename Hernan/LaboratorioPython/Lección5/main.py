5f9b7672a01199ea1cdcb04f5c892ea60a59c1e7
Elher <hervega123@gmail.com>
2023-09-19 01:31:54 -0300

    Añadidos ejercicios 6, 7, 8 y 9 de lección 4, Clase 5, segunda parte

---
+++ Hernan/LaboratorioPython/Lección5/main.py
@@ -0,0 +1,13 @@
+# Comenzamos con funciones
+
+# mi_funcion() No puede llamarse antes de ser definida.
+
+# Definimos una función
+def mi_funcion(): #  Parámetros
+    print("Saludos a todos en la tecnicatura")
+
+mi_funcion() #  Llamamos la función
+mi_funcion() #  Debe definirse la función antes de ser llamada
+mi_funcion() #  Puede llamarse N veces
+
+# Si la función se define en otro módulo, lo que se hace es importar para poder llamarla.
