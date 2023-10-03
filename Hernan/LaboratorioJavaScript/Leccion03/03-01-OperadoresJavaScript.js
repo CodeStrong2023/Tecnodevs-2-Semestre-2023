abdb3c9800a2aa1d625bdee58997c02aec6379a1
Elher <hervega123@gmail.com>
2023-10-03 01:07:43 -0300

    Clase 7, Laboratorio II, JavaScript, Operadores

---
+++ Hernan/LaboratorioJavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,15 @@
+// Ejercicio para encontrar números pares e impares
+let numero = 22;
+if (numero % 2 == 0){
+    console.log("El número es PAR")
+} else {
+    console.log("El número es IMPAR")
+}
+
+// Ejercicio es mayor de edad
+let edad = 18;
+if (edad < 18){
+    console.log("Es mayor de edad")
+} else {
+    console.log("Es mayor de edad")
+}
