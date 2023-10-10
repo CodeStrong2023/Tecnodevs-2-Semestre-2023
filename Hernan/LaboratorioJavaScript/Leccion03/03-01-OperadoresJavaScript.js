007e7cfc2b3a54f1933ba362d16aa84c9ef5809a
Hernán Vega <hervega123@gmail.com>
2023-10-09 22:52:10 -0300

    Clase 8, Laboratorio II, JavaScript, continuación del commit anterior

--- Hernan/LaboratorioJavaScript/Leccion03/03-01-OperadoresJavaScript.js
+++ Hernan/LaboratorioJavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -14,7 +14,7 @@ if (edad < 18){
     console.log("Es mayor de edad")
 }
 
-//Ejercicio: dentro de un rango
+// Ejercicio: dentro de un rango
 let dentroRango = 10; //Aquí vamos a ir cambiando el valor
 let valMin = 0, valMax = 10;
 if (dentroRango >= valMin && dentroRango <= valMax){
@@ -23,3 +23,12 @@ if (dentroRango >= valMin && dentroRango <= valMax){
 else {
     console.log("Esta fuera del rango establecido")
 }
+// Hoy ya no se usa var, se utiliza ley y const
+let nombre2 = "Pedro"; // 
+console.log(nombre2);
+const apellido2 = "Lepes";
+//apellido2 = "Perez"; una constante no puede ser modificada
+console.log(apellido2);
+// let solo puede ser usado dentro de un bloque de llave o función
+// const tiene valores que no pueden ser reasignados
+// var tiene problemas en la estructura if. forma parte del ámbito global
