78d32442398fd3ccc7a62992722bfdf264f87e73
Hernán Vega <hervega123@gmail.com>
2023-10-09 22:25:09 -0300

    Commit desde GitKraken

--- Hernan/LaboratorioJavaScript/Leccion03/03-01-OperadoresJavaScript.js
+++ Hernan/LaboratorioJavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -13,3 +13,13 @@ if (edad < 18){
 } else {
     console.log("Es mayor de edad")
 }
+
+//Ejercicio: dentro de un rango
+let dentroRango = 10; //Aquí vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Esta dentro del rango establecido");
+}
+else {
+    console.log("Esta fuera del rango establecido")
+}
