ea3003fd95cace45471ba774d7d934978bbdb7e9
Emiliano Grossi <emigrossi2004@gmail.com>
2023-10-17 08:27:34 -0300

    Clases de colecciones en Python

--- Emiliano/UTN-Semestres/Python/Leccion4/main.py
+++ Emiliano/UTN-Semestres/Python/Leccion4/main.py
@@ -147,3 +147,92 @@ lista3.sort(reverse=True) #ordenar de manera descendente
 
 #Las tuplas son listas inmutables, es decir, no se pueden modificar
 tuplaEjemplo = (2,"ejem",7.77,"hola"[1,3,4])
+
+
+#Para definir un conjunto
+conjunto2 = set()
+conjunto1 = {}
+conjunto1.add(100)
+conjunto2.add(7)#agregamos 
+conjunto2.add("Hola")
+print(3 not in conjunto1)#Preguntamos si en numero 3 no esta en el conjunto1
+
+#Como hacer la igualdad de dos conjuntos
+print(conjunto1 == conjunto2)
+
+#Opreraciones en conjuntos
+
+# -Union de conjuntos-
+conjunto3 = conjunto1 | conjunto2 #La linea une a los conjuntos
+
+conjunto3 = conjunto1 & conjunto2 #Que elemento tienen en comun
+
+conjunto3 = conjunto1 - conjunto2 #Asigna el valor que esta en el conjunto1 y no en el conjunto2
+
+conjunto3 = conjunto1 ^ conjunto2 #Son los elementos que no comparten y que son diferentes entre ambos
+
+conjunto3 = conjunto1 | conjunto2 
+print(conjunto2.issubset(conjunto3))# Preguntamos si un conjunto es un subconjunto dentro de otro
+print(conjunto1.issubset(conjunto3))
+print(conjunto3.issubset(conjunto2))
+print(conjunto2.issubset(conjunto3))
+print(conjunto3.issubset(conjunto1))# Preguntamos si los elementos del conjunto1 estan dentro del conjunto3
+                                    # si es verdadero quiere decir que el conjunto3 es un superconjunto
+
+# Como saber si ambos conjuntos son disconexos, esto es si no comparten elementos en comun
+print(conjunto1.isdisjoint(conjunto2))
+
+# Convertir un conjunto totalmente en inmutable
+conjunto1 = frozenset #Esto hace que el conjunto sea totalmente inmutable, no se puede agregar
+                      # ni modificar ni eliminar elementos del conjunto 
+
+seleccionArgentina ={
+    10: {"Nombre" : "Lionel Messi", "Edad" : 35, "Altura" : 1.70, "Precio" : "50 Millones", "Posicion" : "Extremo Derecho"},
+    11: {"Nombre" : "Angel Di Maria", "Edad" : 34, "Altura" : 1.80, "Precio" : "12 Millones", "Posicion" : "Extremo Derecho"},
+    24: {"Nombre" : "Paulo Dybala", "Edad" : 28, "Altura" : 1.77, "Precio" : "10 Millones", "Posicion" : "Media Punta"},
+    19: {"Nombre" : "Nicolas Otamendi", "Edad" : 34, "Altura" : 1.83, "Precio" : "7 Millones", "Posicion" : "Defensa Central"},
+    1: {"Nombre" : "Emiliano Martinez", "Edad" : 30, "Altura" : 2.01, "Precio" : "15 Millones", "Posicion" : "Portero"},
+    34: {"Nombre" : "Enzo Fernandez", "Edad" : 24, "Altura" : 1.75, "Precio" : "10 Millones", "Posicion" : "Medio Campo"},
+    15: {"Nombre" : "Julian Alvarez", "Edad" : 23, "Altura" : 1.71, "Precio" : "16 Millones", "Posicion" : "Extremo Derecho"},
+}
+
+for llave, valor in seleccionArgentina.items():
+    print(llave, valor)
+    
+print("En el diccionario hay una cantidad de ",end=" jugadores")
+print(len(seleccionArgentina))
+
+# Pilas usando listas
+pila = [1,2,3]
+
+# Agregar elementos a la pila por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+
+#Sacamos elementos por el final
+elementoBorrado = pila.pop()# Quita el ultimo elemento y lo guarda en la variable
+print(f"Sacamos el elemento {elementoBorrado}")
+print(f"La pila ahora quiedo asi: {pila}")
+
+#Colas con listas
+#Estructura de datos de tipo fifo( first input / first output)
+cola = ["primero","segundo","terceto","cuarto","quinto"]
+
+#Agregamos elementos al final de la cola
+cola.append("sexto")
+
+#Sacamos elementos de la cola
+seRetira = cola.pop(0)
+print(f"Atendido el {seRetira} cliente")
+
+seRetira = cola.pop(0)
+print(f"Atendido el {seRetira} cliente")
+
+seRetira = cola.pop(0)
+print(f"Atendido el {seRetira} cliente")
+
+
+
+
+
