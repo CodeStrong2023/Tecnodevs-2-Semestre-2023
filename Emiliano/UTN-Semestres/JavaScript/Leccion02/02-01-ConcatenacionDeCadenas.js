a2f1437b88d50ffb35bff8c6bd012863afa3eaf3
Emiliano Grossi <emigrossi2004@gmail.com>
2023-10-24 09:57:32 -0300

    Segundo ejercicio con cadenas en Js concatenacion

---
+++ Emiliano/UTN-Semestres/JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,14 @@
+var nombre = "Jose ";
+var apellido = "Gomez";
+var nombreCompleto = nombre+""+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Emiliano"+""+"Grossi";
+console.log(nombreCompleto2);
+var juntos = nombre + 219; //Lee de izquierda a derecha siguiendo la cadena lee el numero como string
+console.log(juntos);
+juntos = nombre + (78 + 12); // Se puede diferenciar a travaz de los parentesis
+console.log(juntos);
+juntos = 79 + 12 + nombre;
+console.log(juntos);
+nombre += apellido; // concatenamos usando el operador simplificado
+console.log(nombre);
