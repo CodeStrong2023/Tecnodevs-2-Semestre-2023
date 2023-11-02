89162b8ec5ec04754f5b41fa55ad001dfe691380
Nicol Paidican <135768019+Nikki-021@users.noreply.github.com>
2023-11-02 17:57:31 -0300

    Java actualizacion
    Agrego ejercicios 6 y 7, pruebas y aritmetica

---
+++ Nicol/Java/Clase 4/Ejercicio07.java
@@ -0,0 +1,25 @@
+// Ejercicio 7: Pedir numero hasta que se introduzca uno negativo
+//              y calcular el promedio
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        int numero, conteo = 0, suma = 0;
+        float promedio = 0;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        while(numero >= 0){
+            suma += numero;
+            conteo++;
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero:"));
+        }
+        if(conteo == 0){
+            JOptionPane.showMessageDialog(null, "Error, la division entre 0 no existe");
+        }
+        else{
+            promedio = (float)suma/conteo;
+            JOptionPane.showMessageDialog(null, "El promedio es "+promedio);
+        }
+    }
+    
+}
