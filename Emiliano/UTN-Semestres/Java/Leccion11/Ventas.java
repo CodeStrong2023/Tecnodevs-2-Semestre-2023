9ed2d1fb5ae8aa8c5153d9b0de5996bfa1b29e7b
Emiliano Grossi <emigrossi2004@gmail.com>
2023-11-08 22:42:48 -0300

    Diseño de clases Clase 13 en Java

---
+++ Emiliano/UTN-Semestres/Java/Leccion11/Ventas.java
@@ -0,0 +1,47 @@
+/* 
+package Java.Leccion11;
+
+public class Ventas {
+    //Atributos de la clase
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+
+    //Contructor vacio
+    private Producto(){
+        this.idProducto = ++Producto.contadorProductos;
+    }
+
+    public Producto(String nombre, double precio){
+        this(); // Llamamos al contructor vacio para el aumento de idProducto
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getidProducto(){
+        return idProducto;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public double getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString(){
+        return "Producto{" + "idProducto= " + idProducto + ",nombre= " + nombre + ",precio= " + precio + "}";
+    }
+}
+*/
---
+++ Emiliano/UTN-Semestres/Java/Leccion11/VentasTest.java
@@ -0,0 +1,46 @@
+/* 
+package Java.Leccion11;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Pantalon", 9500.00);
+        Producto producto2 = new Producto("Campera", 29900.00);
+
+        Orden orden1 = new Orden();
+        //Agregamos productos al arreglo
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOreden();
+
+        //Tarea: Crear mas objetos de tipo Producto(10). Luego de tipo Orden(2)
+        Producto producto3 = new Producto("Riñonera", 500.00);
+        Producto producto4 = new Producto("Remera", 2900.00);
+        Producto producto5 = new Producto("Gorro", 400.00);
+        Producto producto6 = new Producto("Zapatillas", 20000.00);
+        Producto producto7 = new Producto("Ojotas", 4500.00);
+        Producto producto8 = new Producto("Pantalon corto", 9000.00);
+        Producto producto9 = new Producto("Medias", 200.00);
+        Producto producto10 = new Producto("Collar", 100.00);
+        Producto producto11= new Producto("Aros", 50.00);
+        Producto producto12 = new Producto("Pulseras", 50.50);
+
+        Orden orden2 = new Orden();
+        //Agregamos productos al arreglo
+        orden2.agregarProducto(producto3);
+        orden2.agregarProducto(producto4);
+        orden2.agregarProducto(producto5);
+        orden2.agregarProducto(producto6);
+        orden2.agregarProducto(producto7);
+        orden2.mostrarOreden();
+
+        Orden orden3 = new Orden();
+        //Agregamos productos al arreglo
+        orden3.agregarProducto(producto8);
+        orden3.agregarProducto(producto9);
+        orden3.agregarProducto(producto10);
+        orden3.agregarProducto(producto11);
+        orden3.agregarProducto(producto12);
+        orden3.mostrarOreden();
+    }
+}
+*/
