8debadc899209d5f2790f4fb9dc40c80ab8cc75b
Nicol Paidican <135768019+Nikki-021@users.noreply.github.com>
2023-11-10 03:56:24 -0300

    Add files via upload

---
+++ Nicol/Java/Clase 10/Cliente/Cliente.java
@@ -0,0 +1,53 @@
+package domain;
+
+import java.util.Date;
+
+public class Cliente extends Persona{
+    private final int idCliente;
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int contadorCliente;
+    //Constructor
+    public Cliente(String nombre, Date fechaRegistro, boolean vip, char genero, int edad, String direccion) {
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++Cliente.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+
+    //Getter and setter
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro( Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{");
+        sb.append("idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
+        sb.append(", =").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+}
