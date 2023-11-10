3e7e08c1f05f9d0ecf6c9c125226f587457a9acb
Nicol Paidican <135768019+Nikki-021@users.noreply.github.com>
2023-11-10 14:17:04 -0300

    Delete Nicol/Python/Clase 2 directory

--- Nicol/Python/Clase 2/Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-'''
-Ejercicio 1:
-Pasar una expresión matemática a una expresión algorítmica (hacerla en código)
-'''
-a = float(input('Digite el valor de a: '))
-b = float(input('Digite el valor de b: '))
-c = float(input('Digite el valor de c: '))
-resultado = (a ** 3 * (b ** 2 - 2 * a * c)) / (2 * b)
-print(f'El resultado es : {resultado}')
--- Nicol/Python/Clase 2/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-'''
-Determinar la solución lógica de ((3 + 5 x 8 ) < 3 and ((- 6/3 x 4 ) + 2 < 2)) or ( a > b)
-'''
-a = float(input('ingrese el valor de a: ')) # Al final, la veracidad depende del valor de las variables.
-b = float(input('ingrese el valor de b: '))
-resultado = ((3 + 5 * 8 ) < 3 and ((- 6/3 * 4 ) + 2 < 2)) or ( a > b)
-print('El resultado lógico es verdadero') if resultado else print('El resultado lógico es falso')
